b5a1505b46af7c518c5e4f83a2f0681136dd0994
Matt Arnold <mattarnold@u.boisestate.edu>
2019-04-05 13:28:23 -0600

    Almost done with p5.

--- command.c
+++ command.c
@@ -4,30 +4,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include "history.h"
 
 void executeCommand(char* str) {
+	
+	char* args[MAXLINE];
+
 	char* token = strtok(str, " ");
 
-	if (strcmp(token, "exit") == 0) {
-		exit(0);
-	} else if (strcmp(token, "cd") == 0) {
-
+	int i = 0;
+	while (token != NULL) {
+		args[i] = token;
+		i++;
 		token = strtok(NULL, " ");
-		int cdir = chdir(token);
 
-		if (cdir != 0) {
-			perror(token);
-		}
-
-
-	} else {
-		int i = 0;
-		while (token != NULL) {
-			printf("[%d] %s\n", i++, token);
-			token = strtok(NULL, " ");
-		}
 	}
+	if (strcmp(args[0], "exit") == 0) {
+		exit(0);
+	} else if (strcmp(args[0], "cd") == 0) {
+		if (i == 2){
+			int cdir = chdir(args[1]);
 
+			if (cdir != 0) {
+				perror(args[i]);
+			}
 
+		} else {
+			fprintf(stderr, "Invalid Command\n");
+		}
+	} else if (strcmp(args[0], "history") == 0) {
+
+		print_history(i);
+	
+	} else {
+		for (int j = 0; j < i; j++) {
+			printf("[%d] %s\n", j, args[j]);
+		}
+
+	}
 
 }
--- smash.h
+++ smash.h
@@ -2,4 +2,5 @@
  * Function prototype for the executeCommand function.
  * This function will handle parsing the command string.
  */
+#define MAXLINE 4096
 void executeCommand(char* str);
